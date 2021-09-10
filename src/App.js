64195c5baa3100a31d14bba0f2be2fb15f498476
leooladimu <82811737+leooladimu@users.noreply.github.com>
2021-09-10 14:45:38 -0400

    two

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+// import styled, { keyframes } from 'styled-components';
 import axios from "axios";
 import Character from "./components/Character";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-  const [character, setCharacter] = useState([])
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-  useEffect(() => {
+//   // Try to think through what state you'll need for this app before starting. Then build out
+//   // the state properties here.
+//   const [character, setCharacter] = useState([])
+//   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+//   // side effect in a component, you want to think about which state and/or props it should
+//   // sync up with, if any.
+//   useEffect(() => {
+//     axios.get("https://swapi.dev/api/people") 
+//     .then(res => {               
+//       console.log(res)          
+//       setCharacter(res.data)
+//     })
+//     .catch(er => console.log("Error!", er))
+//   }, [])
+
+//   return (
+//     <div className="chr">
+//       <h1 className="Header">Characters</h1>
+//       {character.map(chr => (
+//         <li key={chr.id} chr={chr}>
+//           {chr.name}
+//         </li>
+//        ), [])} 
+//     </div>
+//   );
+// }
+
+// export default App;
+
+const [people, setPeople] = useState([])
+
+    useEffect(() => {
     axios.get("https://swapi.dev/api/people") 
     .then(res => {               
       console.log(res)          
-      setCharacter(res.data)
+      setPeople(res.data)
     })
     .catch(er => console.log("Error!", er))
   }, [])
 
-  return (
-    <div className="chr">
+return (
+    <div className="App">
       <h1 className="Header">Characters</h1>
-      {character.map(chr => (
-        <li key={chr.id} chr={chr}>
-          {chr.name}
-        </li>
-       ), [])} 
+      {people.map(character => (
+        <Character key={character.id} character={character}  propTwo={true} propThree={setPeople} />
+      ), [])}
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,32 @@
-import React from 'react';
+// import React from "react";
+
+// const Character = props => {
+//   console.log(props)
+//   const { character } = props
+//   return (
+//     <div>
+//       <h1>{character}</h1>
+//     </div>
+//   )
+// }
+
+// export default Character;
+
+import React from "react";
 
 const Character = props => {
   console.log(props)
   const { character } = props
-  return (
-    <div>
-      <h1>{character}</h1>
-    </div>
+
+return (
+	<div className="char-list">
+        {/* <h1 className="Header">Characters</h1> */}
+        <li key={character.id} character={character}>
+          {character.name}
+        </li>
+</div>
   )
 }
+
 
 export default Character;
